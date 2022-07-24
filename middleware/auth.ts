1b6001714be6e0daeeb0c4bd3ac25445a2e1da75
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-07-24 04:52:28 -0300

    Changes on auth middleware to check app tokens

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,17 +1,31 @@
 import * as express from 'express';
+import { App } from '../database/models/App';
 
 import { TokenManager } from '../services/TokenManager';
 
-export const AuthMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+export const AuthMiddleware = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
-        res.status(400).send('Token not found');
-        return;
+        const appToken = req.headers['x-app-token'];
+        if (!appToken) {
+            res.status(401).send('Unauthorized');
+            return;
+        } else {
+            const app = await App.findOne({ token: appToken });
+            if (!app) {
+                res.status(401).send('Unauthorized');
+                return;
+            } else {
+                next();
+                return;
+            }
+        }
     }
 
-    const isValid = TokenManager.verify(token);
-    if (!isValid) {
-        res.status(400).send('Invalid token');
+    try {
+        TokenManager.verify(token);
+    } catch (error) {
+        res.status(401).send("Unauthorized");
         return;
     }
 
