64af9b342c3658c29d1dede37fb4ea9cc91a667e
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-07-24 00:47:46 -0300

    Changes on docks and index tests

--- tests/routes/docs.test.ts
+++ tests/routes/docs.test.ts
@@ -1,12 +1,10 @@
 import * as request from 'supertest';
 import app from '../../app';
 
-describe('Successful requests', () => {
-    describe('GET /docs', () => {
-        it('should return a redirect to swagger', async () => {
-            const res = await request(app).get('/docs');
-            expect(res.status).toBe(301);
-            expect(res.header.location).toEqual('/docs/');
-        });
+describe('GET /docs', () => {
+    it('should return a redirect to swagger', async () => {
+        const res = await request(app).get('/docs');
+        expect(res.status).toBe(301);
+        expect(res.header.location).toEqual('/docs/');
     });
 });
