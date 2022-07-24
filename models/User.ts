10b128ebca6ba7ee3fd2951bae06027c20baad52
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-07-24 01:43:51 -0300

    Remove old files

--- models/User.ts
+++
@@ -1,5 +0,0 @@
-import { model } from "mongoose";
-import { IUser } from "../interfaces/IUser";
-import { UserSchema } from "../schemas/UserSchema";
-
-export const User = model<IUser>("users", UserSchema);
--- schemas/TokenInfoSchema.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-import { ITokenInfo } from "../interfaces/ITokenInfo";
-
-export const TokenInfoSchema = new Schema<ITokenInfo>({
-    accessToken: { type: String, required: true },
-    refreshToken: { type: String, required: true },
-    expiresIn: { type: Number, required: true }
-});
-
