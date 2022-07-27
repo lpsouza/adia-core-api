edd52dbc322cb49d13721223044a31b6be7f3858
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-07-27 01:05:11 -0300

    Added HTTPS support

--- server.ts
+++ server.ts
@@ -1,7 +1,12 @@
+import * as https from 'https';
+import * as fs from 'fs';
 import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, (): void => {
+https.createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert')
+}, app).listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
