b6a5503e5abe331e052fb9d3466f5ab38a1d57de
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-07-27 01:49:52 -0300

    Revert "Added HTTPS support"
    This reverts commit edd52dbc322cb49d13721223044a31b6be7f3858.

--- server.ts
+++ server.ts
@@ -1,12 +1,7 @@
-import * as https from 'https';
-import * as fs from 'fs';
 import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
-https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-}, app).listen(PORT, () => {
+app.listen(PORT, (): void => {
     console.log(`Server is running on port ${PORT}`);
 });
