ed6a9630f63abb0e768abb143698ac3e8105db20
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-07-30 08:44:11 -0300

    Added Authorization middleware

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 import * as express from 'express';
 import { DB } from './database/DB';
+
 import { Authentication } from './middleware/Authentication';
+import { Authorization } from './middleware/Authorization';
 
 import routerIndex from './routes/index';
 import routerDocs from './routes/docs';
@@ -16,7 +18,7 @@ app.use(express.json());
 app.use('/', routerIndex);
 app.use('/docs', routerDocs);
 app.use('/auth', routerAuth);
-app.use('/users', Authentication, routerUsers);
-app.use('/apps', Authentication, routerApps);
+app.use('/users', Authentication, Authorization, routerUsers);
+app.use('/apps', Authentication, Authorization, routerApps);
 
 export default app;
